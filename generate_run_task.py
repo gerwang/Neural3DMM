b0e0e816785723202b36f12f3ffe7aa538cd9393
gerwang <gerwang@outlook.com>
2019-10-04 11:18:48 -0400

    add &

--- generate_run_task.py
+++ generate_run_task.py
@@ -1,7 +1,7 @@
 import sys
 
 template = sys.executable + ' neural3DMM.py --nz 50 --name paper_arch_vae --dataset FW_true_10000 --kl ' \
-                            '{} > /dev/null 2>&1 '
+                            '{} > /dev/null 2>&1 &'
 
 with open('tasks.txt', 'w') as fout:
     for i in range(3, 8):
