6b0b0eac9fa269b0aac24b53e1d6548ec1988be1
gerwang <gerwang@outlook.com>
2019-10-10 16:27:21 +0800

    optim to get result

--- test_funcs.py
+++ test_funcs.py
@@ -1,7 +1,33 @@
 import copy
 
 import torch
+import torch.nn as nn
 from tqdm import tqdm
+
+
+def get_optim_points(model, current, target, loss_fn, z, n_iter=1000):
+    params = []
+    for p in model.parameters():
+        if p.requires_grad:
+            p.requires_grad_(False)
+            params.append(p)
+    z = nn.Parameter(z)
+    optim = torch.optim.Adam([z], lr=5e-2)
+    for i in range(n_iter):
+        optim.zero_grad()
+        current = model.decode(z.unsqueeze(0)).squeeze()
+        loss = loss_fn(current, target)
+        # print('iter {}, loss {}'.format(i, loss.item()))
+        loss.backward()
+        optim.step()
+    for p in params:
+        p.requires_grad_(True)
+    return current, z
+
+
+def loss_l1(outputs, targets):
+    L = torch.abs(outputs - targets).mean()
+    return L
 
 
 def test_autoencoder_dataloader(device, model, dataloader_test, shapedata, mm_constant=1000):
@@ -15,6 +41,9 @@ def test_autoencoder_dataloader(device, model, dataloader_test, shapedata, mm_co
             tx = sample_dict['points'].to(device)
             tx_dict = model(tx)
             prediction = tx_dict['rec']
+            prediction = torch.cat([
+                get_optim_points(model, prediction[i], tx[i], loss_l1, tx_dict['mu'][i])[0].unsqueeze(0)
+            ], dim=0)
             if i == 0:
                 predictions = copy.deepcopy(prediction)
             else:
