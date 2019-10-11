0bc13a895a878312a5fda8038ea37c25cb20c9a1
gerwang <gerwang@outlook.com>
2019-10-11 12:31:23 +0800

    runTask

--- generate_run_task.py
+++ generate_run_task.py
@@ -1,11 +1,11 @@
 import sys
 
-template = sys.executable + ' neural3DMM.py --nz 50 --name paper_arch_vae --dataset FW_true_10000 --kl ' \
-                            '{} > /dev/null 2>&1 &'
+template = sys.executable + ' neural3DMM.py --nz 50 --name paper_arch_vae_{0} --dataset FW_true_10000 --kl ' \
+                            '{0} > /dev/null 2>&1 &'
 
 with open('tasks.txt', 'w') as fout:
-    for i in range(3, 8):
-        for j in range(1, 9):
+    for i in range(5, 10):
+        for j in range(1, 10, 3):
             kl = j * 10 ** -i
             cmd = template.format(kl)
             fout.write(cmd + '\n')
